8d36c3d4c52a965b2bb0001699220a77e8071de3
phifour <w.frisch@gmx.at>
2021-05-06 22:53:15 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,6 @@
-from flask import Flask, jsonify, abort, request
-from werkzeug.exceptions import HTTPException
-from flask_cors import CORS
-#from sklearn import tree
-
+from flask import Flask
 app = Flask(__name__)
-CORS(app)
-app.config["DEBUG"] = True
 
-@app.errorhandler(Exception)
-def handle_error(e):
-    code = 500
-    if isinstance(e, HTTPException):
-        code = e.code
-    return jsonify(error=str(e)), code
-
-@app.route('/test', methods=['GET'])
-def getMessage():
-    data = {'message': 'Hello Test!'}
-    return jsonify(data)
-
-app.run(debug=False)
+@app.route("/")
+def hello():
+    return "Hello, World!"
